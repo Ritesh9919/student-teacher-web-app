1684e2e05d6641bc9e18f5696372eb6867b6b97d
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-12-06 14:03:58 +0530

    bug fixes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://student-teacher-api-a1no.onrender.com//api",
+  baseURL: "https://student-teacher-api-a1no.onrender.com/api",
 });
 
 export default api;
